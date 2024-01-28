79b85a97cca4464065584758b35749522cfe07fe
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-01-28 06:00:53 -0300

    added player choice

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+getPlayerChoice();
+getComputerChoice();
 
-let computerChoice = Math.random() * 100;
-computerChoice = Math.floor(computerChoice) + 1;
-if (computerChoice => 33) {
-    alert("computer picked rock");
-}
-if(computerChoice > 34 && computerChoice >= 67){
-    alert("computer picked paper");
-}
-if(computerChoice => 68 && computerChoice < 100){
-    alert("computer picked scissor");
+function getPlayerChoice() {
+    playerChoice = prompt("pick rock, paper or scissor");
+    if (playerChoice == "rock") {
+        alert("you picked rock");
+    }
+    if (playerChoice == "paper") {
+        alert("you picked paper");
+    }
+    if (playerChoice == "scissor") {
+        alert("you picked scissor");
+    }
+    return playerChoice;
 }
 
+function getComputerChoice() {
+    let computerChoice = Math.random() * 100;
+    computerChoice = Math.floor(computerChoice) + 1;
+    if (computerChoice <= 33) {
+        alert("computer picked rock");
+    }
+    if (computerChoice > 34 && computerChoice <= 67) {
+        alert("computer picked paper");
+    }
+    if (computerChoice > 68) {
+        alert("computer picked scissor");
+    }
+    return computerChoice;
+}
 
 
