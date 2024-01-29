1ce183ec6dca0eb7b7e19919cda4b966218b1859
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-01-29 14:38:16 -0300

    updated index js

--- index.js
+++ index.js
@@ -1,54 +1,37 @@
+
 getPlayerChoice();
 getComputerChoice();
 startRound();
 
  function startRound(playerChoice,computerChoice){
      let roundWinner;
-     const roundDraw = new Boolean(false);
+
      if (playerChoice > computerChoice) {
          roundWinner = playerChoice;
-     }
-     if (playerChoice = computerChoice) {
-         roundDraw = true;
      }
      if (playerChoice < computerChoice) {
          roundWinner = computerChoice;
      }
-     if (roundDraw); {
-         alert("Draw!");
-     }
-     if (playerChoice=!computerChoice){
-         alert("The winner is " + roundWinner + " .");
-     }
-     return;
+     return alert("The winner is " + roundWinner + " .");
  }
 
 
 function getPlayerChoice() {
     let playerChoice = prompt("pick rock, paper or scissor");
-    if (playerChoice == "rock") { playerChoice <=1
+    if (playerChoice == "rock") {playerChoice == 1
     }
-    if (playerChoice == "paper") { playerChoice > 1 && playerChoice <= 2
+    if (playerChoice == "paper") {playerChoice == 2
     }
-    if (playerChoice == "scissor") {playerChoice = 3
+    if (playerChoice == "scissor") {playerChoice == 3
     }
-    alert("You picked " + playerChoice);
-    return playerChoice;
+    return alert("You picked " + playerChoice);
 }
 
 function getComputerChoice() {
-    let computerChoice = Math.random() * 3;
-    computerChoice = Math.floor(computerChoice) + 1;
-    if (computerChoice <= 1) {
-        alert("Computer picked rock")
-    }
-    else if (computerChoice > 1 && computerChoice <= 2) {
-        alert("Computer picked paper")
-    }
-    else if (computerChoice = 3) { 
-        alert("Computer picked scissor")
-    }
-    return computerChoice;
+    let computerChoice = Math.floor(Math.random()*3);
+    if (computerChoice == 1) {return "rock"}
+    else if (computerChoice == 2) {return "paper"}
+    else if (computerChoice == 3) {return "scissor" }
 }
 
 
